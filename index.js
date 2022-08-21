47fdacdf57e1bfd0a6ed3db840cdbab9182b7101
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-08-21 04:43:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  
+  const newTutorials = tutorials.map(letter => {
+    const words = letter.split(' ');
+    const wordsCapitalized = words.map(word => word[0].toUpperCase() + word.slice(1, word.length));
+    const finalTutorials = wordsCapitalized.join(' ');
+  return finalTutorials;
+});
+return newTutorials;
 }
